b054a3bacf783645e91f9d7ed91544f19695111a
nikitoqq <nikita.rodishevskiy@gmail.com>
2025-01-04 06:11:15 +0300

    working

--- src/utils/math..js
+++ src/utils/math..js
@@ -5,14 +5,10 @@ export const answer = (value) => {
       break;
     }
     i++;
-    const arrValue = value.match(
-      /-?\(?\d+(\.\d+)?\)?[+\-*/√^]-?\(?\d+(\.\d+)?\)?/g
-    );
+    const arrValue = value.match(/-?\(?\d+\)?[+\-*/√^]-?\(?\d+\)?/g);
     if (value.includes("(")) {
-      const bracketValue = value.match(
-        /\(-?\d+(\.\d+)?\s*[+\-*/√^]\s*-?\d+(\.\d+)?\)/g
-      );
-      value = operationInBrackets(bracketValue);
+      const [pattern, replacement] = findBrackets(value);
+      value = value.replace(pattern, replacement);
     } else if (arrValue !== null) {
       value = operation(value);
     } else {
@@ -22,6 +18,12 @@ export const answer = (value) => {
   return value;
 };
 
+const findBrackets = (value) => {
+  const pattern = /\([^()]*\)/;
+  const replacement = operation(value.match(pattern)[0].slice(1, -1));
+  return [pattern, replacement];
+}
+
 const operation = (value) => {
   let i = 0;
   while (true) {
@@ -29,7 +31,6 @@ const operation = (value) => {
       break;
     }
     i++;
-    console.log(value);
     if (value.includes("√")) {
       value = funcRoot(value);
     } else if (value.includes("^")) {
@@ -52,40 +53,6 @@ const operation = (value) => {
       value.includes("+") &&
       !/-?\(?\d+(\.\d+)?\)?[-]-?\(?\d+(\.\d+)?\)?/g.test(value)
     ) {
-      value = funcPlus(value);
-    } else {
-      break;
-    }
-  }
-  return value;
-};
-
-const operationInBrackets = (value) => {
-  let i = 0;
-  while (true) {
-    if (i > 10) {
-      break;
-    }
-    i++;
-    if (value.includes("√")) {
-      value = funcRoot(value);
-    } else if (value.includes("^")) {
-      value = funcPow(value);
-    } else if (
-      value.includes("*") &&
-      value.indexOf("*") <
-        (value.indexOf("/") === -1 ? 100 : value).indexOf("/")
-    ) {
-      value = funcMultiply(value);
-    } else if (
-      value.includes("/") && value.indexOf("/") < value.indexOf("*") === -1
-        ? 100
-        : value.indexOf("*")
-    ) {
-      value = funcDevision(value);
-    } else if (/-?\(?\d+(\.\d+)?\)?[-]-?\(?\d+(\.\d+)?\)?/g.test(value)) {
-      value = funcMinus(value);
-    } else if (value.includes("+")) {
       value = funcPlus(value);
     } else {
       break;
@@ -123,10 +90,8 @@ const funcMinus = (value) => {
 
 const funcMultiply = (value) => {
   const arrValue = value.match(/-?\(?\d+(\.\d+)?\)?[\*]-?\(?\d+(\.\d+)?\)?/g);
-  console.log(`arrValue: ${arrValue}`);
   arrValue.map((item) => {
     const operands = item.match(/-?[0-9]+(\.[0-9]+)?/g);
-    console.log(`operands: ${operands}`);
     value = value.replace(item, multiply(+operands[0], +operands[1]));
   });
 
@@ -147,7 +112,6 @@ const funcRoot = (value) => {
   const arrValue = value.match(/\(?\d+(\.\d+)?\)?[\√]\(?\d+(\.\d+)?\)?/g);
   arrValue.map((item) => {
     const operands = item.match(/-?[0-9]+(\.[0-9]+)?/g);
-    console.log(operands);
     value = value.replace(item, root(+operands[0] || 2, +operands[1]));
   });
 
